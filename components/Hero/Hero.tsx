a36dbdd1fca4906a0db623945fae22ba7144a407
Vivian Xu <hi@vivianxu.com>
2022-12-25 10:22:38 +1100

    Hero component

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,17 @@
+export default function Hero() {
+  return (
+    <section>
+      <div className="container">
+        <h1>Welcome to Oly's website</h1>
+        <h2>What I do</h2>
+        <p>
+          ux, ui, visual design, research, strategy, education, design,
+          branding, workshops and more...
+        </p>
+        <p>
+          <a href="https://www.linkedin.com/in/olysu/">view full cv</a>
+        </p>
+      </div>
+    </section>
+  );
+}
