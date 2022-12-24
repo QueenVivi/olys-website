810abd4afca7ab7cd146066a585e41c36df221c4
Vivian Xu <hi@vivianxu.com>
2022-12-25 10:18:30 +1100

    Footer component

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <p>all works are copyright Oly Su 2022</p>
+      </div>
+    </footer>
+  );
+}
