77a85deeb31cba614fb2992615a3d47184a6b8bd
Vivian Xu <hi@vivianxu.com>
2022-12-25 10:05:32 +1100

    Header component

---
+++ components/Header/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            Oly Su
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
