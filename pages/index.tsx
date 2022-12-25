91be8b6b77932deffa0b3985de961d0daaddc8cf
Vivian Xu <hi@vivianxu.com>
2022-12-25 12:46:18 +1100

    Import Bootstrap global Scss

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,12 +13,6 @@ export default function Home() {
       <Head>
         <title>Oly Su's Website</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-          crossOrigin="anonymous"
-        />
       </Head>
       <Header />
       <main>
