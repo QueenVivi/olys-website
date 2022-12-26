48a0076e532232f37d1f41c9373fb220910666a4
Vivian Xu <hi@vivianxu.com>
2022-12-27 10:54:52 +1100

    Ladder block styling

--- pages/[workId].tsx
+++ pages/[workId].tsx
@@ -17,54 +17,65 @@ export default function Work({
   const otherImages = images.slice(2);
   return (
     <Layout>
-      <div className="container">
-        <article>
+      <article>
+        <div className="container">
           <h1>{title}</h1>
           <Hero
             eyebrow="challenge"
             heading={challenge.title}
             message={challenge.content}
           />
-          <div className="container">
-            <div className="row">
-              <div className="col-6">
-                <h2>{scope.title}</h2>
-                <p>{scope.content}</p>
-              </div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>{scope.title}</h2>
+              <p>{scope.content}</p>
             </div>
-            <div className="row">
-              <div className="col-6">
-                <h2>{problem.title}</h2>
-                <p>{problem.content}</p>
-                <h2>{process.title}</h2>
-                <p>{process.content}</p>
-              </div>
-              <div className="col-6">
-                <img src={firstImage.src} alt={firstImage.alt} />
-              </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>{problem.title}</h2>
+              <p>{problem.content}</p>
+              <h2>{process.title}</h2>
+              <p>{process.content}</p>
             </div>
-            <div className="row">
-              <div className="col-6">
-                <img src={secondImage.src} alt={secondImage.alt} />
-              </div>
-              <div className="col-6">
-                <h2>{outcomes.title}</h2>
-                <p>{outcomes.content}</p>
-                <h2>{future.title}</h2>
-                <p>{future.content}</p>
-              </div>
+            <div className="col-md-6">
+              <img
+                className="img-fluid"
+                src={firstImage.src}
+                alt={firstImage.alt}
+              />
+            </div>
+          </div>
+          <div className="row d-flex">
+            <div className="col-md-6">
+              <img
+                className="img-fluid"
+                src={secondImage.src}
+                alt={secondImage.alt}
+              />
+            </div>
+            <div className="col-md-6">
+              <h2>{outcomes.title}</h2>
+              <p>{outcomes.content}</p>
+              <h2>{future.title}</h2>
+              <p>{future.content}</p>
             </div>
           </div>
           {otherImages.map((image, i) => {
             return (
               <>
-                <img key={i} src={image.src} alt={image.alt} />
+                <img
+                  key={i}
+                  src={image.src}
+                  alt={image.alt}
+                  className="img-fluid"
+                />
                 <p>{image.caption}</p>
               </>
             );
           })}
-        </article>
-      </div>
+        </div>
+      </article>
     </Layout>
   );
 }
