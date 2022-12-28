9a85ae83758d7491680d575c334c01c73639c470
Vivian Xu <hi@vivianxu.com>
2022-12-28 17:18:37 +1100

    Styling Hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,10 +1,11 @@
+import Eyebrow from "../Eyebrow";
+
 export default function Hero(props) {
   return (
     <section>
-      <div className="container">
-        <h1>{props.eyebrow}</h1>
-        <h2>{props.heading}</h2>
-        <p className="display-6">{props.message}</p>
+      <div className="container py-8 py-sm-10">
+        <Eyebrow eyebrow={props.eyebrow} heading={props.heading} />
+        <p className="display-6 lh-lg text-white">{props.message}</p>
         {props.linkUrl && props.linkText ? (
           <p>
             <a href={props.linkUrl}>{props.linkText}</a>
