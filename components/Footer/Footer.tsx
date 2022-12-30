17a501bb8dd3c7f7987250207c9054c5a6bd6a85
Vivian Xu <hi@vivianxu.com>
2022-12-30 22:50:43 +1100

    Fix: remove console.log and margin tweak

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,8 @@
 export default function Footer() {
   const currentYear = new Date().getFullYear();
-  console.log(currentYear);
   return (
     <footer>
-      <div className="container mb-6">
+      <div className="container my-6">
         <p>all works are copyright Oly Su {currentYear}</p>
       </div>
     </footer>
